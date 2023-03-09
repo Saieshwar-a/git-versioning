b6e7381010893365c445d5757baea412060029f1
A <saieshwar.a@siemens-healthineers.com>
2023-03-09 11:52:11 +0530

    console app added to the main branch

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter operation");
+string op =(Console.ReadLine());
+Console.WriteLine("number");
+float x = float.Parse(Console.ReadLine());
+Console.WriteLine("number");
+float y = float.Parse(Console.ReadLine());
+
+
+double res;
+switch (op)
+{
+    case "+":
+        res = x + y;
+        break;
+    case "-":
+        res = x - y;
+        break;
+    case "*":
+        res = x * y;
+        break;
+    case "/":
+        res = x / y;
+        break;
+    case "%":
+        res = x / y;
+        break;
+    default:
+        throw new Exception("please enter valid input");
+}
+Console.WriteLine(res); 
